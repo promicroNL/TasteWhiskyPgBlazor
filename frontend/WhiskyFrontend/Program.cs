332e2abbb5c323f4fea663f5ec8ad231e797d947
Tonie <chaosonline@gmail.com>
2025-07-04 10:57:28 +0200

    Fixed http client implemenation. Now waiting on Codec to implement new features

--- frontend/WhiskyFrontend/Program.cs
+++ frontend/WhiskyFrontend/Program.cs
@@ -5,6 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+    
+builder.Services.AddHttpClient("NotesAPI", client =>
+{
+    client.BaseAddress = new Uri("http://localhost:5122/");
+});
 
 var app = builder.Build();
 
